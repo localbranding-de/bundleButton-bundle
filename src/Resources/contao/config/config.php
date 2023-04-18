cb687bd588c5b8710e5c7924f160a1c804be4fe5
Frank Jeroma <53892311+localbranding-de@users.noreply.github.com>
2023-04-18 09:55:21 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -6,18 +6,18 @@ if(\Input::get('do') == 'lb_bundles')
     
     
     $GLOBALS['TL_JAVASCRIPT'][] = '/assets/jquery/js/jquery.min.js';
-    $GLOBALS['TL_JAVASCRIPT'][] = '/bundles/extendedproductdetail/js/lb_be_bundle.js';
-   // $GLOBALS['TL_CSS'][] = '/bundles/extendedproductdetail/css/lb_be_font.css';
+    $GLOBALS['TL_JAVASCRIPT'][] = '/bundles/bundlebutton/js/lb_be_bundle.js';
+   // $GLOBALS['TL_CSS'][] = '/bundles/bundlebutton/css/lb_be_font.css';
     //$GLOBALS['TL_CSS'][] = '/files/theme_lb_demoshop/css/lb-default.css';
     
 }
 
 // Frontend modules
-$GLOBALS['FE_MOD']['LocalBranding Module']['productButton'] = 'LocalbrandingDe\ExtendedProductDetailBundle\Module\ProductButtonModule'; 
-$GLOBALS['FE_MOD']['LocalBranding Module']['bundleButton'] = 'LocalbrandingDe\ExtendedProductDetailBundle\Module\BundleButtonModule';
+$GLOBALS['FE_MOD']['LocalBranding Module']['productButton'] = 'LocalbrandingDe\BundleButtonBundle\Module\ProductButtonModule'; 
+$GLOBALS['FE_MOD']['LocalBranding Module']['bundleButton'] = 'LocalbrandingDe\BundleButtonBundle\Module\BundleButtonModule';
 
 
-$GLOBALS['FE_MOD']['LocalBranding Module']['bundleHandler'] = 'LocalbrandingDe\ExtendedProductDetailBundle\Module\BundleHandlerModule';
+$GLOBALS['FE_MOD']['LocalBranding Module']['bundleHandler'] = 'LocalbrandingDe\BundleButtonBundle\Module\BundleHandlerModule';
 
 
 array_insert($GLOBALS['BE_MOD'], 1,
