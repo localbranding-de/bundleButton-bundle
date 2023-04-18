ed0ddee2422d5a342970674899a376c5a6b0522e
Frank Jeroma <53892311+localbranding-de@users.noreply.github.com>
2023-04-18 10:34:19 +0200

    Create tl_ls_shop_variant.php

---
+++ src/Resources/contao/languages/de/tl_ls_shop_variant.php
@@ -0,0 +1,42 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_inPriceHeader1'] = array('Preisfeld Headline 1' , 'Bitte Headline 1 eingeben für Akkordeon im Preisfeld (optional)');
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_inPriceHeader2'] = array('Preisfeld Headline 2' , 'Bitte Headline 2 eingeben für Akkordeon im Preisfeld (optional)');
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_inPriceHeader3'] = array('Preisfeld Headline 3' , 'Bitte Headline 3 eingeben für Akkordeon im Preisfeld (optional)');
+
+
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_inPriceText1'] = array('Preisfeld Text 1' , 'Bitte Text 1 eingeben für Akkordeon im Preisfeld (optional)');
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_inPriceText2'] = array('Preisfeld Text 2' , 'Bitte Text 2 eingeben für Akkordeon im Preisfeld (optional)');
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_inPriceText3'] = array('Preisfeld Text 3' , 'Bitte Text 3 eingeben für Akkordeon im Preisfeld (optional)');
+
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_productTab1'] = array('Produkt-Tab 1' , 'Bitte Benennung für Tab 1 eingeben (optional)');
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_productTab2'] = array('Produkt-Tab 2' , 'Bitte Benennung für Tab 2 eingeben (optional)');
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_productTab3'] = array('Produkt-Tab 3' , 'Bitte Benennung für Tab 3 eingeben (optional)');
+
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_productTab1_text'] = array('Tab Text' , 'Bitte Text für Akkordeon-Abschnitt angeben (optional)');
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_productTab2_text'] = array('Tab Text' , 'Bitte Text für Akkordeon-Abschnitt angeben (optional)');
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_productTab3_text'] = array('Tab Text' , 'Bitte Text für Akkordeon-Abschnitt angeben (optional)');
+
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_productTab1_accordList'] =  array('Akkordeon für Tab','Bitte hier Akkordeons für das Frontend anlegen.');
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_productTab2_accordList'] =  array('Akkordeon für Tab','Bitte hier Akkordeons für das Frontend anlegen.');
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_productTab3_accordList'] =  array('Akkordeon für Tab','Bitte hier Akkordeons für das Frontend anlegen.');
+
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_sellingUnit'] = array("Verkaufseinheit", 'Bitte angeben, in welcher Menge das Produkt verkauft wird. Z. B. 12 bei Hosting, wenn dieses als Monatspreis angegeben wird, aber nur als ganzes Jahr verkauft wird');
+
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_consultantShare'] = array('Kontakt-Anteil',"Bitte Anteil von Netto-VKP (ohne Multiplikation mit VK-Einheit) für Consultant in Währung eingeben.");
+
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_timeContractNoticePeriodInDays'] = array('Zeitvertrag Kündigungsfrist',"Bei Zeitvertrag: Bitte Kündigungsfrist in Tagen angeben.");
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_timeContractPeriodInDays'] = array('Zeitvertrag Periode',"Bei Zeitvertrag: Bitte Vertragsintervall in Tagen angeben.");
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_revisionLoops'] = array('Korrekturschleifen',"Bitte eingeben, wieviele Korrekturschleifen im Preis inbegriffen sind.");
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_hasMaxOneJob'] = array('Maximal 1 Job',"Bitte angeben, ob für das Produkt maximal 1 Job angelegt werden soll. (Beispiel Domain-Hosting: Besteht aus 12 Monaten (Einheiten), darf aber nur maximal einen Job erhalten.)");
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_isDownloadProduct'] = array('Download-Produkt',"Bitte angeben, ob das Produkt ein reines Download-Produkt ist.");
+
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_dependantProduct'] = array('Abhängiges Produkt','Ist die Verwendung des Produkts abhängig von einem anderen Produkt? Beispiel: Ein Website-Designpaket kann nur gebucht werden, wenn dazu vorher Hosting und Website gebucht wurden.');
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_timeContract'] = array('Zeitvertrag','Bitte angeben, ob es sich bei dem Produkt um einen Zeitvertrag handelt.');
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_dependantProductID'] = array('Produkt-ID','Bitte angeben, von welchem Produkt die Abhängigkeit besteht.');
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_isConsulting'] = array('Produkt ist Kunden-Termin mit frei wählbarem Datum' , 'Bitte angeben, ob das Produkt ein Termin ist, der von Kunden an einem variablen, vorher nicht festgelegten Datum gebucht werden kann.');
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_Duration'] = array('Dauer der Dienstleistung','Bitte angeben, wie lang ein Termin für die Erfüllung dieser Dienstleistung dauern soll.');
+
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_isDiscount'] = array('Ist Bundle Bonus-Gutschein',"Bitte angeben, ob das Produkt ein Bonus-Gutschein in einem Bundle ist.");
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_cancellationPossible'] = array('Widerruf möglich',"Bitte angeben, ob das Produkt nach dem Kauf stornierbar ist.");
+$GLOBALS['TL_LANG']['tl_ls_shop_variant']['lb_isObjectRent'] = array('Objekt-Vermietung','Bitte angeben, ob das Produkt die Vermietung eines Objektes ist.');
